56777c214cdbab8a34e8c89e84ea135b4774295d
Jakub Kupczyk <jkbkupczyk@gmail.com>
2020-11-28 17:53:55 +0100

    Added Swagger2 support

--- src/main/java/com/kupczyk/jokesapi/JokesApiApplication.java
+++ src/main/java/com/kupczyk/jokesapi/JokesApiApplication.java
@@ -2,8 +2,10 @@ package com.kupczyk.jokesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class JokesApiApplication {
 
 	public static void main(String[] args) {
