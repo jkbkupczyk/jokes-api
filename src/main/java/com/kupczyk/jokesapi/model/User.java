10b4ec12d744d3045012e973cce6921dfd313fe8
Jakub Kupczyk <jkbkupczyk@gmail.com>
2020-11-28 17:55:07 +0100

    Created User class for joke providers

---
+++ src/main/java/com/kupczyk/jokesapi/model/User.java
@@ -0,0 +1,18 @@
+package com.kupczyk.jokesapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String first;
+    private String last;
+}
